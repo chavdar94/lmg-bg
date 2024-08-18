ed51aa57f95c95548ccd435f06db822b786ad304
Chavdar <ch.tonchev94@gmail.com>
2024-08-18 13:06:13 +0300

    add services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,62 +1,57 @@
-import { Computer } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Service, ServiceCategory } from "@prisma/client";
+import db from "@/lib/client";
+import { ChevronRightIcon } from "lucide-react";
 
-function ServicesPage() {
+async function ServicesPage() {
+  const [serviceCategories, services] = await Promise.all([
+    db.serviceCategory.findMany(),
+    db.service.findMany(),
+  ]);
+
   return (
-    <section className="flex flex-col justify-center items-center bg-[#fafafa] p-10">
-      <h1 className="text-center text-4xl md:text-5xl font-bold uppercase mb-2">
-        Нашите услуги
+    <>
+      <h1 className="text-center text-4xl font-semibold uppercase my-4">
+        Предлагани услуги
       </h1>
-      <section className="flex flex-col md:flex-row flex-wrap justify-center gap-4 mt-4">
-        <article className="min-h-[220px] max-w-[300px] p-8 flex justify-center items-center flex-col bg-[#fff]  shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out">
-          <Computer size={30} className="m-5 text-[#ff5742]" />
-          <h2 className="mb-3 font-normal text-center text-2xl">Title</h2>
-          <p className="text-[#6c757d] text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-            sapiente!
-          </p>
-        </article>
-        <article className="min-h-[220px] max-w-[300px] p-8 flex justify-center items-center flex-col bg-[#fff]  shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out">
-          <Computer size={30} className="m-5 text-[#ff5742]" />
-          <h2 className="mb-3 font-normal text-center text-2xl">Title</h2>
-          <p className="text-[#6c757d] text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-            sapiente!
-          </p>
-        </article>
-        <article className="min-h-[220px] max-w-[300px] p-8 flex justify-center items-center flex-col bg-[#fff]  shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out">
-          <Computer size={30} className="m-5 text-[#ff5742]" />
-          <h2 className="mb-3 font-normal text-center text-2xl">Title</h2>
-          <p className="text-[#6c757d] text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-            sapiente!
-          </p>
-        </article>
-        <article className="min-h-[220px] max-w-[300px] p-8 flex justify-center items-center flex-col bg-[#fff]  shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out">
-          <Computer size={30} className="m-5 text-[#ff5742]" />
-          <h2 className="mb-3 font-normal text-center text-2xl">Title</h2>
-          <p className="text-[#6c757d] text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-            sapiente!
-          </p>
-        </article>
-        <article className="min-h-[220px] max-w-[300px] p-8 flex justify-center items-center flex-col bg-[#fff]  shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out">
-          <Computer size={30} className="m-5 text-[#ff5742]" />
-          <h2 className="mb-3 font-normal text-center text-2xl">Title</h2>
-          <p className="text-[#6c757d] text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-            sapiente!
-          </p>
-        </article>
-        <article className="min-h-[220px] max-w-[300px] p-8 flex justify-center items-center flex-col bg-[#fff]  shadow-xl hover:shadow-2xl transition-all duration-300 ease-in-out">
-          <Computer size={30} className="m-5 text-[#ff5742]" />
-          <h2 className="mb-3 font-normal text-center text-2xl">Title</h2>
-          <p className="text-[#6c757d] text-center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima,
-            sapiente!
-          </p>
-        </article>
-      </section>
-    </section>
+      <Accordion type="single" collapsible className="w-full mt-12">
+        {serviceCategories.map((category: ServiceCategory) => (
+          <AccordionItem value={category.id} key={category.id}>
+            <AccordionTrigger>{category.title}</AccordionTrigger>
+            <AccordionContent>
+              <ul className="w-full">
+                {services
+                  .filter(
+                    (service: Service) => service.categoryId === category.id
+                  )
+                  .map((service) => (
+                    <li
+                      key={service.id}
+                      className="flex justify-between px-2 py-1.5 text-slate-500"
+                    >
+                      <div className="flex justify-center items-center">
+                        <ChevronRightIcon className="w-4 h-4" />
+                        <span>{service.title}</span>
+                      </div>
+                      <span>
+                        {service.price > 0
+                          ? `${service.price} лв.`
+                          : "Безплатно"}
+                      </span>
+                    </li>
+                  ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
   );
 }
+
 export default ServicesPage;
